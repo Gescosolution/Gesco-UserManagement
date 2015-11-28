346a84ad6b331c0ebaa1ca72f2cedf19c34e26c6
Abel Josué Francisco Agra <jfrancisco4490@gmail.com>
2015-11-28 21:30:46 +0100

    Eliminados archivos de configuracion de LDAP

--- config/deploy/install_ldap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ -z ${OPENSHIFT_NODEJS_IP+x} ]; then 
-	echo "Despliegue en OpenShift? No..."; 
-else 
-	apt-get install ldap-utils
-	apt-get install slapd
-fi
--- config/deploy/run_ldap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir /tmp/slapd
-slapd -f config/ldap/slapd.conf -h ldap://localhost:3389 &
-sleep 3
-ldapadd -h localhost:3389 -D cn=admin,dc=localhost -w test -f models/local/ldap/ldap_empresa.ldif
-ldapadd -h localhost:3389 -D cn=admin,dc=localhost -w test -f models/local/ldap/ldap_oficinas.ldif
-ldapadd -h localhost:3389 -D cn=admin,dc=localhost -w test -f models/local/ldap/ldap_roles.ldif
-ldapadd -h localhost:3389 -D cn=admin,dc=localhost -w test -f models/local/ldap/ldap_empleados.ldif
