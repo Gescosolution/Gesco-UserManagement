af76bc88da4ce23b176e1380d1c32b36633c0e3f
Abel Josué Francisco Agra <jfrancisco4490@gmail.com>
2015-11-28 18:09:53 +0100

    Incluida ejecucion de scripts de configuracion tras instalacion y despliegue de la aplicacion

--- config/deploy/install_ldap.sh
+++ config/deploy/install_ldap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt-get install ldap-utils
-apt-get install slapd
+if [ -z ${OPENSHIFT_NODEJS_IP+x} ]; then 
+	echo "Despliegue en OpenShift? No..."; 
+else 
+	apt-get install ldap-utils
+	apt-get install slapd
+fi
